72fc8bdd96f9a7e80c9083f151f0b6cbb88e6df0
Luã Maury Maquiné da Silva <83655458+luamaquine@users.noreply.github.com>
2022-02-08 22:52:59 -0400

    JOptionPane and comments
    Was added:
    - JOptionPane
    - Comments
    - Java.Swing

--- Dawkins-Weasel-Program/AplicacaoAGJava/src/aplicacaoagjava/classes/WParent.java
+++ Dawkins-Weasel-Program/AplicacaoAGJava/src/aplicacaoagjava/classes/WParent.java
@@ -1,18 +1,18 @@
 package aplicacaoagjava.classes;
 /**
  *
- * @author danysimas
+ * @author danysimas / luamaquine
  */
 public class WParent {
-    
+    // classe referente ao individuo
     private String genome ="";
-
+    // Geração de genomas aleatorios
     public WParent(int genomeLenght) {
         for(int i =0; i< genomeLenght; i++){
             genome += randomGene();
         }
     }
-
+    //Aplicação das mutaçoes para as proximas gerações
      public WParent(String parentGenome) {
          for(int i =0; i<parentGenome.length();i++){
              if(aplicacaoagjava.AplicacaoAGJava.MUTATION_RATE>=Math.random()){
@@ -27,6 +27,7 @@ public class WParent {
          return genome;
      }
      
+    //Retorna o maior score do individuo que seria accuracy
      public int accuracy(){
          int accuracyScore =0;
          for(int i=0; i<aplicacaoagjava.AplicacaoAGJava.TARGET.length(); i++){
@@ -37,7 +38,7 @@ public class WParent {
          }
          return accuracyScore;
      }
-    
+    //Retornando char de um gene que fica na string de "genes" a partir de um randomIndex
     public static char randomGene(){
         int randomIndex = (int) Math.floor(Math.random()*aplicacaoagjava.AplicacaoAGJava.GENES.length());
         return aplicacaoagjava.AplicacaoAGJava.GENES.charAt(randomIndex);
